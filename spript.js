f39fceae4b971b3068f712a9b5ff1362632d1b09
dapugliese <dapugliese@gmail.com>
2025-03-29 20:14:39 -0300

    sin la alerta

--- spript.js
+++ spript.js
@@ -3,7 +3,7 @@
 function mostrarDato() {
     
     const input = document.getElementById("inputDato");
-    alert(input.value);
+
     const parrafo = document.getElementById("resultado");
     parrafo.innerText = "El dato ingresado es: " + input.value;
 
