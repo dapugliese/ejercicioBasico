b10b224a8777b91357244b589d68f620c7bff7a7
dapugliese <dapugliese@gmail.com>
2025-03-29 16:50:25 -0300

    ejercicioBasico

---
+++ spript.js
@@ -0,0 +1,13 @@
+
+
+function mostrarDato() {
+    
+    const input = document.getElementById("inputDato");
+    alert(input.value);
+    const parrafo = document.getElementById("resultado");
+    parrafo.innerText = "El dato ingresado es: " + input.value;
+
+}
+
+
+document.getElementById("botonMostrar").onclick = mostrarDato;
