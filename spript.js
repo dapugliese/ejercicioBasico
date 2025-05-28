25bd162c217ac597121568f4972161747df21407
Diego Apugliese <dapugliese@gmail.com>
2025-05-27 21:39:47 -0300

    ejercicio vector

--- spript.js
+++ spript.js
@@ -21,9 +21,51 @@ function mostrarDato() {
 var suma = 0;
 
 
+var lista = [];
 
+function ingresar(){
+    var numero = document.getElementById('input-Dato');
+    
+    lista.push(parseInt(numero.value));
+    var p = document.getElementById('resultado');
+    p.innerText=lista;
+}
 
+function Finalizar(){
 
+    var v1 = document.getElementById("menor");
+    var v2 = document.getElementById("mayor");
+    var v3 = document.getElementById("promedio");
+    var v4 = document.getElementById("suma_total");
+
+    var min = lista[0];
+    var max = 0;
+    var promedio = 0;
+    var suma = 0;
+
+    for (var i=0; i<lista.length; i++){
+    suma = suma+lista[i];
+
+    if (lista[i]>max) {
+        max = lista[i];
+
+    }
+
+    if (lista[i]<min) { 
+        min = lista[i];
+
+    }
+     
+    promedio = suma / lista.length;
+
+    }
+
+    v2.innerText += " " + max;
+    v4.innerText += " " + suma;
+    v1.innerText += " " + min
+    v3.innerText += " " + promedio
+
+}
 
 
 // document.getElementById("botonMostrar").onclick = mostrarDato;
